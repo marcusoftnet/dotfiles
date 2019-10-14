575e962c0fadcd48f413bb42e14160fb7112f052
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-10-14 15:42:38 +0200

    Added alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias please='sudo'
+
+alias test-admin="cd ~/Projects/salt/appliedtechnology/salt-jsfs-test-admin-automation/"
