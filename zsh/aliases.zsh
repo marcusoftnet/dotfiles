a347c908e67fe3a0b2cc446604485ebcf2aef5be
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-10-20 21:28:16 +0200

    Added a lot of .. shortcuts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,13 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias please='sudo'
 
-alias places-projects="cd ~/Projects/"
-alias places-blog="cd ~/Projects/blog"
-alias places-appliedtechnology="cd ~/Projects/salt/appliedtechnology/"
-alias places-test-admin="cd ~/Projects/salt/appliedtechnology/salt-jsfs-test-admin-automation/"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+alias go-projects="cd ~/Projects/"
+alias go-blog="cd ~/Projects/blog"
+alias go-appliedtechnology="cd ~/Projects/salt/appliedtechnology/"
+alias go-test-admin="cd ~/Projects/salt/appliedtechnology/salt-jsfs-test-admin-automation/"
