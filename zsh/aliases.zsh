e3878fcc91e2a3267f5d7e814541769d6d9fefc4
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-12-18 13:09:37 +0100

    Adds new alisases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,13 +12,16 @@ alias ......="cd ../../../../.."
 
 alias go-projects="cd ~/Projects/"
 alias go-blog="cd ~/Projects/blog"
+
 alias go-appliedtechnology="cd ~/Projects/salt/appliedtechnology/"
-alias go-slides-jsfs="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-material-slides/"
-alias go-docs-jsfs="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-material-docs"
+alias go-docs="cd ~/Projects/salt/appliedtechnology/salt-material-docs"
 
 alias go-jsfs="cd ~/Projects/salt/appliedtechnology/jsfs/"
-alias go-dnfs="cd ~/Projects/salt/appliedtechnology/dnfs/"
+alias go-jsfs-test-admin="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-tool-testAdmin/"
+alias go-jsfs-slides="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-material-slides/"
+alias go-jsfs-docs="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-material-docs"
 
-alias go-test-admin="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-tool-testAdmin/"
-alias go-slides="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-material-slides/"
-alias go-docs="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-material-docs"
+alias go-dnfs="cd ~/Projects/salt/appliedtechnology/dnfs/"
+alias go-dnfs-test-admin="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-tool-testAdmin/"
+alias go-dnfs-slides="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-material-slides/"
+alias go-dnfs-docs="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-material-docs"
