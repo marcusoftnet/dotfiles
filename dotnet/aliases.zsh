989c0a47d57c58da5ab4912ec9e6e11cd443ed5d
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-02-18 18:00:01 +0100

    Adds a dni-alias

---
+++ dotnet/aliases.zsh
@@ -0,0 +1,1 @@
+alias dni='curl https://dotnet.microsoft.com/download/dotnet-core/scripts/v1/dotnet-install.sh -o ~/.dotnet/dotnet-install.sh -s | chmod +x ~/.dotnet/dotnet-install.sh | ~/.dotnet/dotnet-install.sh'
