a660eb0edeb47eedd9c7f44d548b5b2dc356a3c2
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-07-02 18:09:02 +0200

    Adds a few things for java

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,16 +15,11 @@ alias go-blog="cd ~/Projects/blog"
 
 alias go-appliedtechnology="cd ~/Projects/salt/appliedtechnology/"
 alias go-docs="cd ~/Projects/salt/appliedtechnology/salt-material-docs"
+alias go-test-admin="cd ~/Projects/salt/appliedtechnology/salt-test-admin"
 
 alias go-jsfs="cd ~/Projects/salt/appliedtechnology/jsfs/"
-alias go-jsfs-test-admin="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-tool-testAdmin/"
-alias go-jsfs-slides="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-material-slides/"
-alias go-jsfs-docs="cd ~/Projects/salt/appliedtechnology/jsfs/jsfs-material-docs"
-
 alias go-dnfs="cd ~/Projects/salt/appliedtechnology/dnfs/"
-alias go-dnfs-test-admin="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-tool-testAdmin/"
-alias go-dnfs-slides="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-material-slides/"
-alias go-dnfs-docs="cd ~/Projects/salt/appliedtechnology/dnfs/dnfs-material-docs"
+alias go-jfs="cd ~/Projects/salt/appliedtechnology/jfs/"
 
 alias do-clone-repo="sh ~/Projects/salt/appliedtechnology/salt-material-docs/scripts/cloneRepository.sh"
 alias do-flatten-repo="sh ~/Projects/salt/appliedtechnology/salt-material-docs/scripts/flattenRepoHistory.sh "
