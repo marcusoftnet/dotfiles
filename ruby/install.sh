569cfa495ff56213b5a01813a7cb25040adc8d36
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-08-21 12:00:04 +0200

    Adds ruby and jekyll

---
+++ ruby/install.sh
@@ -0,0 +1,7 @@
+ruby-install ruby
+
+echo "source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh" >> ~/.zshrc
+echo "source $(brew --prefix)/opt/chruby/share/chruby/auto.sh" >> ~/.zshrc
+echo "chruby ruby-3.1.2" >> ~/.zshrc # run 'chruby' to see actual version
+
+gem install jekyll
