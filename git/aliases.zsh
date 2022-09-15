fe07537f10365a140038006e861c610ffc84d5a3
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-09-15 08:45:27 +0200

    Adds gaca

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,6 +3,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 
+
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
@@ -15,4 +16,3 @@ alias gs='git status -sb'
 alias gac='git add -A && git commit -m'
 alias gaca='git add -A && git commit --amend --no-edit'
 alias gstatusall="find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;"
-
