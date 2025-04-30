f2b6dcdecabab48d5778bd1f2af8dcecacf5d57a
Marcus Hammarberg <marcusoft.net@gmail.com>
2025-04-30 10:39:45 +0200

    Adds a speak-webpage command

---
+++ functions/speak-webpage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: speak-webpage <URL>"
+  exit 1
+fi
+
+URL="$1"
+
+echo "Fetching and processing webpage..."
+TEXT=$(curl -Ls "$URL" | lynx -stdin -dump -nolist | iconv -f utf-8 -t utf-8 -c)
+
+echo "Speaking..."
+say "$TEXT"
