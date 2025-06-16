78a19795462e13eeea0815993c66a49c68b3c827
Marcus Hammarberg <marcusoft.net@gmail.com>
2025-06-16 09:45:34 +0200

    Fixing some output

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,6 +1,8 @@
 export NVM_DIR=~/.nvm
 if [ -e "$(brew --prefix nvm)/nvm.sh" ]; then
   source $(brew --prefix nvm)/nvm.sh
-  nvm use node --silent
+  if command -v npm >/dev/null 2>&1; then
+    nvm use node --silent
+  fi
 fi
 
