8dbfc64403a7e7ae1b4a43d98d3ef9022c7e1f69
Marcus Hammarberg <marcus.hammarberg@umain.com>
2026-06-10 22:19:50 +0200

    Added alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,13 +13,7 @@ alias ......="cd ../../../../.."
 alias go-projects="cd ~/Projects/"
 alias go-blog="cd ~/Projects/blog"
 
-alias go-appliedtechnology="cd ~/Projects/salt/appliedtechnology/"
-alias go-docs="cd ~/Projects/salt/appliedtechnology/salt-material-docs"
-alias go-test-admin="cd ~/Projects/salt/appliedtechnology/salt-tool-testAdmin"
-
-alias go-jsfs="cd ~/Projects/salt/appliedtechnology/jsfs/"
-alias go-dnfs="cd ~/Projects/salt/appliedtechnology/dnfs/"
-alias go-jfs="cd ~/Projects/salt/appliedtechnology/jfs/"
+alias go-frankly="cd ~/Projects/eidra/frankly"
 
 alias do-clone-repo="sh ~/Projects/salt/appliedtechnology/salt-material-docs/scripts/cloneRepository.sh"
 alias do-flatten-repo="sh ~/Projects/salt/appliedtechnology/salt-material-docs/scripts/flattenRepoHistory.sh "
